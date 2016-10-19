0f62dce897597b96c5cf57877a085b146495113b
Emanoel Victor <victoremanoel23@gmail.com>
2016-10-19 14:43:32 -0300

    Adição do ficheiro de Carregar/Mostrar imagem.

---
+++ LoadDispImg.cpp
@@ -0,0 +1,30 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, char **argv)
+{
+	if (argc != 2)
+	{
+	 cout << "Usage: display_Image ImageToLoadAndDisplay" << endl;
+	 return -1;
+	}
+
+	Mat image;
+	image = imread(argv[1], CV_LOAD_IMAGE_COLOR);	//Lê o arquivo
+
+	if (!image.data)				//Verifica se a entrada é valida.
+	{
+	 cout << "Could not open or find the image" << std::endl;
+	 return -1;
+	}
+
+	namedWindow( "Display window", WINDOW_AUTOSIZE);	//Cria uma janela para mostrar a imagem.
+	imshow ( "Display window", image);			//Mostra a imagem dentro
+
+	waitKey(0);
+	return 0
+}
