2d25c5425293ad23f84e9bb69077fd0adb49faf0
esousacosta <victoremanoel23@gmail.com>
2021-02-28 16:14:23 -0300

    Added a function to regions.cpp to get the coords of the neg. reg.

--- pixel-manipulation/src/regions.cpp
+++ pixel-manipulation/src/regions.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <opencv2/core.hpp>
 #include <opencv2/core/matx.hpp>
 #include <opencv2/opencv.hpp>
 #include <ostream>
@@ -7,25 +8,25 @@
 
 void get_coordinates(std::vector<int> &point1, std::vector<int> &point2)
 {
+  // This function needs some streamlining. A lisp macro would do the job. 
   int temp;
   const char* points[] = {"P(x)", "P(y)"};
   std::cout << "Please provide the requested coordinate" << std::endl;
-  std::cout << "P1(X): " << std::endl;
-  std::cin >> temp;
-  point1.push_back(temp);
-  std::cout << "P1(Y): " << std::endl;
-  std::cin >> temp;
-  point1.push_back(temp); 
-  std::cout << "P2(X): " << std::endl;
-  std::cin >> temp;
-  point2.push_back(temp); 
-  std::cout << "P2(Y): " << std::endl;
-  std::cin >> temp;
-  point2.push_back(temp); 
+  for (int i=0; i<2; i++){
+	std::cout << "P( " << (i==0?"X):":"Y):") << std::endl;
+	std::cin >> temp;
+	point1.push_back(temp);
+  }
+  for (int i=0; i<2; i++){
+	std::cout << "P( " << (i==0?"X):":"Y):") << std::endl;
+	std::cin >> temp;
+	point2.push_back(temp);
+  }
 }
 
 int main(int argc, char** argv){
   cv::Mat image;
+  cv::Mat negative_image;
   cv::Vec3b val;
   std::vector<int> p1(2,0);
   std::vector<int> p2(2,0);
@@ -42,14 +43,19 @@ int main(int argc, char** argv){
 
   cv::namedWindow("Window", cv::WINDOW_AUTOSIZE);
 
+  cv::Vec3b white;
+  white[0] = 255;
+  white[1] = 255;
+  white[2] = 255;
+  
 
-  for(int i=200;i<210;i++){
-    for(int j=10;j<200;j++){
-      image.at<uchar>(i,j)=0;
+  for(int i=p1[0];i<p1[1];i++){
+    for(int j=p1[1];i<p1[1];i++){
+	  image.at<cv::Vec3b>(i,j) = white - image.at<cv::Vec3b>(i,j);
     }
   }
 
-  cv::imshow("janela", image);
+  cv::imshow("Negative", image);
   cv::waitKey();
 
   image = cv::imread("../figuras/bolhas.png",cv::IMREAD_COLOR);
