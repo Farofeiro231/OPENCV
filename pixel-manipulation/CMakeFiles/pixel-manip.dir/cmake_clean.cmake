4a0663b6530928ad6cd0689ac55483d8a94b5478
esousacosta <victoremanoel23@gmail.com>
2021-02-28 11:28:27 -0300

    Configured de CMakeLists file and fixed a typo in pixel-manip.cpp

---
+++ pixel-manipulation/CMakeFiles/pixel-manip.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pixel-manip.dir/src/pixel-manip.cpp.o"
+  "pixel-manip"
+  "pixel-manip.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pixel-manip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
