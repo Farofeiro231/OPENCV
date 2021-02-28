e2549e652cf7dcb5835f9202e04c917aacef870f
esousacosta <victoremanoel23@gmail.com>
2021-02-28 12:16:26 -0300

    Added a new .cpp (pixels) and modified the Cmakelists; added figs.

---
+++ pixel-manipulation/CMakeFiles/pixels.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pixels.dir/src/pixels.cpp.o"
+  "pixels"
+  "pixels.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pixels.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
