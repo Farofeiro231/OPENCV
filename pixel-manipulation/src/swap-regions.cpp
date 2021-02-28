bcb96b3f699e24dd89767203cac035efa08d1fc6
esousacosta <victoremanoel23@gmail.com>
2021-02-28 20:47:11 -0300

    Tried to implement the regions swap feature by simply fliping th img

--- pixel-manipulation/src/swap-regions.cpp
+++ pixel-manipulation/src/swap-regions.cpp
@@ -1,6 +1,9 @@
+#include <opencv2/calib3d.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/core/hal/interface.h>
+#include <opencv2/core/mat.hpp>
 #include <opencv2/core/matx.hpp>
+#include <opencv2/core/types.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <ostream>
@@ -8,16 +11,19 @@
 
 void swap_image(cv::Mat &src_image, cv::Mat &out_image)
 {
-  cv::Mat aux;
   int rows = src_image.rows;
   int cols = src_image.cols;
-  aux = src_image;
+  cv::Mat diagonal = cv::Mat::eye(rows, cols, CV_32F);
+  cv::Mat anti_diagonal;
+  cv::flip(src_image, out_image, -1);
   cv::Mat top_left = src_image({0, cols/2}, {0, rows/2});
   cv::Mat top_right = src_image({0, cols/2}, {rows/2+1, rows-1});
   cv::Mat bottom_left = src_image({cols/2+1, cols-1}, {0, rows/2});
   cv::Mat bottom_right = src_image({cols/2+1, cols-1}, {rows/2+1, rows-1});
   // Swapped image's top left
-  out_image({0, cols/2}, {0, rows/2}) = bottom_right; 
+  //out_image(cv::Range(0, cols/2), cv::Range(0, rows/2)) = bottom_right.clone(); 
+  cv::imshow("Window", out_image);
+  cv::waitKey();
   // Swapped image's bottom right
   out_image({cols/2+1, cols-1}, {rows/2+1, rows-1}) = top_left; 
   // Swapped image's top right
@@ -33,7 +39,6 @@ void swap_image(cv::Mat &src_image, cv::Mat &out_image)
 int main(int argc, char** argv){
   cv::Mat image;
   cv::Mat swapped_image;
-  cv::Vec3b val;
 
   image = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
   if(!image.data){
