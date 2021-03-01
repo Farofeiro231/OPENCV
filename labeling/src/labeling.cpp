f492d03b382f872d14c3e250267e0bac73062e70
esousacosta <victoremanoel23@gmail.com>
2021-03-01 11:41:59 -0300

    Implement a contour-finding function, but yet to isolate the desired

--- labeling/src/labeling.cpp
+++ labeling/src/labeling.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <opencv2/core/hal/interface.h>
+#include <opencv2/core/matx.hpp>
+#include <opencv2/core/types.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 using namespace cv;
 
@@ -40,16 +43,33 @@ void eliminate_edge_objects(cv::Mat &image)
 
 int main(int argc, char** argv){
   cv::Mat image, realce;
+  cv::Mat contoured;
   int width, height;
   int nobjects;
 
+  // Taken from the documentation. Contours is a list of all the image's contours, and hierarchy
+  // tells me which contours have children (i.e.: have other contours inside of them).
+
+  std::vector<std::vector<cv::Point>> contours;
+  std::vector<cv::Vec4i> hierarchy;
+  cv::Scalar scalar_red = {100, 255, 100};
   cv::Point p;
+
   image = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
 
   if(!image.data){
     std::cerr << "The image didn't load correctly. Exiting...\n";
     return(-1);
   }
+
+  contoured = image;
+  for (int i=0; i < contours.size(); i+=1){
+	cv::findContours(contoured, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+	cv::drawContours(contoured, contours, i, scalar_red, 2, cv::LINE_8, hierarchy, 0);
+  }
+
+
+  cv::imshow("Contoured image", contoured);
 
   width=image.cols;
   height=image.rows;
@@ -71,7 +91,7 @@ int main(int argc, char** argv){
         p.x=j;
         p.y=i;
   		// preenche o objeto com o contador
-		  cv::floodFill(image,p,nobjects);
+		cv::floodFill(image,p,nobjects);
       }
     }
   }
