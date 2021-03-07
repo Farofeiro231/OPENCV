2a1babbc735f00628fc8decf7aaa795ce2ec84ea
esousacosta <victoremanoel23@gmail.com>
2021-03-07 17:47:30 -0300

    Adding a focused-window height and width; changing the appro. once +

--- space-filtering/src/addweighted.cpp
+++ space-filtering/src/addweighted.cpp
@@ -55,6 +55,7 @@ int main(int argvc, char** argv){
   int upper_increment = 0;
   int lower_increment = 255;
   int unfocus_width = 100;
+  int unfocus_height = 100;
   int unfocus_intensity = 50;
   int offset = 0;
   cv::Mat alpha_matrix(image1.rows, image1.cols, CV_8UC1, cv::Scalar(255, 255, 255));//CV_8UC1, cv::Scalar(255,255,255));
@@ -63,24 +64,26 @@ int main(int argvc, char** argv){
   int _stride = alpha_matrix.step;
   std::uint8_t *matrix_data = alpha_matrix.data;
   cv::imshow("Alpha matrix original", alpha_matrix);
+
   // This offset variable is used to compensate the bigger black region at
   // the bottom of the mask created if the user picks too small a intensity for
   // the given unfocus_widht.
-  offset = std::max(image1.rows - unfocus_width, 0);
+
+  offset = std::max(image1.rows - unfocus_width - unfocus_intensity, 0)/2;
   for (int i=0; i<image1.rows; i++){
 	uint8_t* p = alpha_matrix.ptr(i);
 	// Add 1 step to the increments given the value of i
 	// meets the correct conditions.
 	if (i + offset < unfocus_width)
 	  upper_increment = upper_increment + floor(255.0/unfocus_intensity);
-	if(i >= (alpha_matrix.rows - unfocus_width))
+	if (i >= (alpha_matrix.rows - unfocus_width))
 	  lower_increment = lower_increment - floor(255.0/unfocus_intensity);
 	for (int j=0; j<image1.cols; j++){
-	  if(i < unfocus_width) {
+	  if(i < std::min(unfocus_height - unfocus_width/2, 0)) {
 		*p++ = std::min(upper_increment, 255);
 		std::cout << "(i, j): " << i << ", " << j << std::endl;
 	  }
-	  else if(i >= (alpha_matrix.rows - unfocus_width)){
+	  else if (i >= std::max(unfocus_height + unfocus_width/2, 255)) {
 		*p++ = std::max(lower_increment, 0);
 		std::cout << "(i, j): " << i << ", " << j << std::endl;
 	  }
