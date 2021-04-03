679a1de6a461027e95e36b910e8249fea175d7f1
Bacamartes <victoremanoel23@gmail.com>
2021-04-03 15:23:33 -0300

    Fixed the upper portion of the mask's center; bottom to go

--- space-filtering/src/video_manip.cpp
+++ space-filtering/src/video_manip.cpp
@@ -49,8 +49,13 @@ void modify_mask(cv::Mat &mask)
   // I used the matrix_data approach because I wanted to see if there was a noticeable difference
   // in speed by using that (in contrast with the at<> method).
 
-  upper_step =  (1.0/(focus_height - focus_width/2.0)) * focus_intensity;
-  lower_step =  (1.0/(mask.rows - (focus_height + focus_width/2.0))) * focus_intensity;
+  if ((focus_height - focus_width/2.0) >=0)
+	upper_step =  (1.0/(focus_width/2.0)) * focus_intensity;
+  else
+	upper_step = (1.0 / focus_height) * focus_intensity;
+   
+  // lower_step =  (1.0/(mask.rows - (focus_height + focus_width/2.0))) * focus_intensity;
+  lower_step =  (1.0/(focus_height + focus_width/2.0)) * focus_intensity;
 
   // The focus region has the following characterístics: it's centered around the focus_height and it has
   // the remaining of the image outside [focus_height - focus_width/2, focus_height + focus_width/2]
