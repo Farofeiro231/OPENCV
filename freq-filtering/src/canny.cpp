2074adbe12b0d0b774a35db992f1d3744db2dbcd
Bacamartes <victoremanoel23@gmail.com>
2021-04-06 17:24:03 -0300

    Added the dotilism function to the canny program

--- freq-filtering/src/canny.cpp
+++ freq-filtering/src/canny.cpp
@@ -2,23 +2,87 @@
 #include <opencv2/core.hpp>
 #include <opencv2/core/base.hpp>
 #include "opencv2/opencv.hpp"
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 using namespace cv;
+
+#define STEP 5
+#define JITTER 3
+#define RAIO 3
 
 int top_slider = 10;
 int top_slider_max = 200;
 
 char TrackbarName[50];
 
-Mat image, border;
+Mat image, border, art;
+
+cv::Mat dotilism(cv::Mat& image){
+  vector<int> yrange;
+  vector<int> xrange;
+
+  Mat frame, points;
+
+  int width, height, gray;
+  int x, y;
+
+  width=image.size().width;
+  height=image.size().height;
+
+  xrange.resize(height/STEP);
+  yrange.resize(width/STEP);
+
+  iota(xrange.begin(), xrange.end(), 0);
+  iota(yrange.begin(), yrange.end(), 0);
+
+  for(uint i=0; i<xrange.size(); i++){
+	xrange[i]= xrange[i]*STEP+STEP/2;
+  }
+
+  for(uint i=0; i<yrange.size(); i++){
+	yrange[i]= yrange[i]*STEP+STEP/2;
+  }
+
+  points = Mat(height, width, CV_8U, Scalar(255));
+
+  random_shuffle(xrange.begin(), xrange.end());
+
+  for(auto i : xrange) {
+	random_shuffle(yrange.begin(), yrange.end());
+	for(auto j : yrange) {
+	  x = i+rand()%(2*JITTER)-JITTER+1;
+	  y = j+rand()%(2*JITTER)-JITTER+1;
+	  gray = image.at<uchar>(x,y);
+	  circle(points,
+			 cv::Point(y,x),
+			 RAIO,
+			 CV_RGB(gray,gray,gray),
+			 -1,
+			 LINE_AA);
+	}
+  }
+
+  imwrite("pontos.jpg", points);
+  return points;
+}
+
 
 void on_trackbar_canny(int, void*){
   Canny(image, border, top_slider, 3 * top_slider);
-  cv::normalize(border, border, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+  // cv::normalize(border, border, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
   std::cout << "Border: " << border << std::endl;
-  imshow("canny", border);
+  art = dotilism(image);
+  // imshow("canny", image.mul(border));
+  imshow("canny", art + border);
 }
+
 
 int main(int argc, char**argv){
   int width, height;
