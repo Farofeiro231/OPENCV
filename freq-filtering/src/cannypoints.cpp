32d6e810e45614a0c06f9e977733f0fc66c9e8cf
Bacamartes <victoremanoel23@gmail.com>
2021-04-06 17:26:17 -0300

    Added a few control parameters for the cannny art

--- freq-filtering/src/cannypoints.cpp
+++ freq-filtering/src/cannypoints.cpp
@@ -14,8 +14,11 @@ using namespace std;
 using namespace cv;
 
 #define STEP 5
+#define EDGE_STEP 2
 #define JITTER 3
-#define RAIO 3
+#define EDGE_JITTER 1
+#define RAIO 4
+#define EDGE_RAIO 2
 
 int top_slider = 10;
 int top_slider_max = 200;
