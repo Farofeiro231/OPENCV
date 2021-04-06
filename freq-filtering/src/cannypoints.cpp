498a44054bb6d1838c1a65692a6a88a47b563060
Bacamartes <victoremanoel23@gmail.com>
2021-04-06 17:38:20 -0300

    Modified the final art to add smaller circles

--- freq-filtering/src/cannypoints.cpp
+++ freq-filtering/src/cannypoints.cpp
@@ -4,6 +4,8 @@
 #include "opencv2/opencv.hpp"
 #include <fstream>
 #include <iomanip>
+#include <opencv2/core/types.hpp>
+#include <opencv2/imgproc.hpp>
 #include <vector>
 #include <algorithm>
 #include <numeric>
@@ -69,6 +71,71 @@ cv::Mat dotilism(cv::Mat& image){
 			 CV_RGB(gray,gray,gray),
 			 -1,
 			 LINE_AA);
+	}
+  }
+
+  imwrite("pontos.jpg", points);
+  return points;
+}
+
+
+cv::Mat edge_dotilism(cv::Mat& image){
+  vector<int> yrange;
+  vector<int> xrange;
+
+  Mat frame, points;
+  static Mat new_image, border_image;
+
+  int width, height, gray;
+  int x, y;
+
+  width=image.size().width;
+  height=image.size().height;
+
+  xrange.resize(height/STEP);
+  yrange.resize(width/STEP);
+
+  iota(xrange.begin(), xrange.end(), 0);
+  iota(yrange.begin(), yrange.end(), 0);
+
+  Canny(image, border, top_slider, 3 * top_slider);
+  cv::normalize(border, border, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+  
+  for(uint i=0; i<xrange.size(); i++){
+	xrange[i]= xrange[i]*STEP+STEP/2;
+  }
+
+  for(uint i=0; i<yrange.size(); i++){
+	yrange[i]= yrange[i]*STEP+STEP/2;
+  }
+
+  points = Mat(height, width, CV_8U, Scalar(255));
+
+  random_shuffle(xrange.begin(), xrange.end());
+
+  for(auto i : xrange) {
+	random_shuffle(yrange.begin(), yrange.end());
+	for(auto j : yrange) {
+	  x = i+rand()%(2*JITTER)-JITTER+1;
+	  y = j+rand()%(2*JITTER)-JITTER+1;
+	  gray = image.at<uchar>(x,y);
+	  circle(points,
+			 cv::Point(y,x),
+			 RAIO,
+			 CV_RGB(gray,gray,gray),
+			 -1,
+			 LINE_AA);
+	}
+  }
+
+  new_image = border_image.mul(border);
+
+  for (int i = 0; i < new_image.rows; i++) {
+	for (int j = 0; j < new_image.cols; j++) {
+	  if (new_image.at<uchar>(i, j) != 0) {
+		gray = new_image.at<uchar>(i, j);
+		circle(points, cv::Point(j, x), EDGE_RAIO, CV_RGB(gray, gray, gray), -1, LINE_AA);
+	  }
 	}
   }
 
