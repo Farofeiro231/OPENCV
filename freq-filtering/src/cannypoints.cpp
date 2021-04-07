d8e0316bc5b2052dc2187a694ab0d5a24715db54
Bacamartes <victoremanoel23@gmail.com>
2021-04-06 22:16:22 -0300

    streamlined the cannypoints function; removed unnec. commts.

--- freq-filtering/src/cannypoints.cpp
+++ freq-filtering/src/cannypoints.cpp
@@ -29,55 +29,6 @@ char TrackbarName[50];
 
 Mat image, border, art;
 
-cv::Mat dotilism(cv::Mat& image){
-  vector<int> yrange;
-  vector<int> xrange;
-
-  Mat frame, points;
-
-  int width, height, gray;
-  int x, y;
-
-  width=image.size().width;
-  height=image.size().height;
-
-  xrange.resize(height/STEP);
-  yrange.resize(width/STEP);
-
-  iota(xrange.begin(), xrange.end(), 0);
-  iota(yrange.begin(), yrange.end(), 0);
-
-  for(uint i=0; i<xrange.size(); i++){
-	xrange[i]= xrange[i]*STEP+STEP/2;
-  }
-
-  for(uint i=0; i<yrange.size(); i++){
-	yrange[i]= yrange[i]*STEP+STEP/2;
-  }
-
-  points = Mat(height, width, CV_8U, Scalar(255));
-
-  random_shuffle(xrange.begin(), xrange.end());
-
-  for(auto i : xrange) {
-	random_shuffle(yrange.begin(), yrange.end());
-	for(auto j : yrange) {
-	  x = i+rand()%(2*JITTER)-JITTER+1;
-	  y = j+rand()%(2*JITTER)-JITTER+1;
-	  gray = image.at<uchar>(x,y);
-	  circle(points,
-			 cv::Point(y,x),
-			 RAIO,
-			 CV_RGB(gray,gray,gray),
-			 -1,
-			 LINE_AA);
-	}
-  }
-
-  imwrite("pontos.jpg", points);
-  return points;
-}
-
 
 cv::Mat edge_dotilism(cv::Mat& image){
   vector<int> yrange;
@@ -144,12 +95,8 @@ cv::Mat edge_dotilism(cv::Mat& image){
 
 
 void on_trackbar_canny(int, void*){
-  Canny(image, border, top_slider, 3 * top_slider);
-  // cv::normalize(border, border, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
-  std::cout << "Border: " << border << std::endl;
   art = edge_dotilism(image);
-  // imshow("canny", image.mul(border));
-  imshow("canny", art + border);
+  imshow("canny", art);
 }
 
 
